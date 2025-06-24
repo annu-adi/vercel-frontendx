33ccae25724e95573fcceec172a48d3570b70e40
annu-adi <rashmi.maurya10@gmail.com>
2025-06-24 20:14:14 +0530

    add

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -12,13 +12,13 @@ import {BsFillCheckCircleFill} from "react-icons/bs";
 function Home() {
     const [todos, setTodos]= useState([])
     useEffect(()=>{
-    axios.get('http://localhost:3001/get')
+    axios.get('https://vercel-backendx.vercel.app/get')
     .then(result =>setTodos(result.data))
     .catch(err => console.log(err))
     },[])
 
     const handleEdit = (id) => {
-        axios.put('http://localhost:3001/update/'+id)
+        axios.put('https://vercel-backendx.vercel.app/update/'+id)
         .then(result =>{
            location.reload()
         })
@@ -26,7 +26,7 @@ function Home() {
     }
 
     const handleDelete = (id) => {
-        axios.delete('http://localhost:3001/delete/'+id)
+        axios.delete('https://vercel-backendx.vercel.app/delete/'+id)
         .then(result =>{
            location.reload()
         })
